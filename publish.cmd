a51df8520c589ec5f2a221cf45db55bf7628ea07
foxe22 <chloe.foxe99@gmail.com>
2020-02-19 13:04:18 +0000

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=election-bot-20-Web-Deploy.pubxml -p:Password=uXkP63pnY4efiNtsJK2vGwyZmP8e1vCbEpwSsRjyviAaq96EPvkhzaz9d5N5
+
