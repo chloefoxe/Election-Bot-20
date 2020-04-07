588966faacd082f65ec478370f160080f1806624
foxe22 <chloe.foxe99@gmail.com>
2020-04-08 00:01:06 +0100

    Update EndConversationDialog.cs

--- Dialogs/EndConversationDialog.cs
+++ Dialogs/EndConversationDialog.cs
@@ -1,12 +1,13 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs.Choices;
 
 namespace Microsoft.BotBuilderSamples.Dialogs
 {
