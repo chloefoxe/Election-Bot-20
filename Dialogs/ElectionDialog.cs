bda26099f7fcf8fce422dfc6040523d059e70778
foxe22 <chloe.foxe99@gmail.com>
2020-04-07 12:31:51 +0100

    Update ElectionDialog.cs

--- Dialogs/ElectionDialog.cs
+++ Dialogs/ElectionDialog.cs
@@ -31,27 +31,30 @@ namespace Microsoft.BotBuilderSamples.Dialogs
 
         private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            var personalDetails = (PersonalDetails)stepContext.Options;
+            // var personalDetails = (PersonalDetails)stepContext.Options;
             
-            if (personalDetails.Voted == null)
-            {
+            // if (personalDetails.Voted == null)
+            // {
                 var messageText = "So, alot has happened since this year's general election then! Did you vote in February last?";
                 var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
                 return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
-            }
+            //}
             
-            var luisResult = await _luisRecognizer.RecognizeAsync<Luis.ElectionBot>(stepContext.Context, cancellationToken);
-            return await stepContext.NextAsync(personalDetails, cancellationToken);
+            // var luisResult = await _luisRecognizer.RecognizeAsync<Luis.ElectionBot>(stepContext.Context, cancellationToken);
+            // return await stepContext.NextAsync(personalDetails, cancellationToken);
         }
 
         private async Task<DialogTurnResult> AskVotedAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            var personalDetails = (PersonalDetails)stepContext.Options;
+            // var personalDetails = (PersonalDetails)stepContext.Options;
+            // var luisResult = await _luisRecognizer.RecognizeAsync<Luis.ElectionBot>(stepContext.Context, cancellationToken);
+
             var luisResult = await _luisRecognizer.RecognizeAsync<Luis.ElectionBot>(stepContext.Context, cancellationToken);
+            
             //var personalDetails = (PersonalDetails)stepContext.Options;
-            string[] votedString, didNotVoteString;
-            votedString = new string[]{ "Did not Vote"};
-            didNotVoteString = new string[]{ "Did Vote"};
+            // string[] votedString, didNotVoteString;
+            // votedString = new string[]{ "Did not Vote"};
+            // didNotVoteString = new string[]{ "Did Vote"};
 
             switch (luisResult.TopIntent().intent)
             {
