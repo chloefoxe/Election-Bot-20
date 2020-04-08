db66eb8069b0a8d205433c9d35333f60356a4f2a
foxe22 <chloe.foxe99@gmail.com>
2020-04-08 11:28:17 +0100

    Update DialogAndWelcomeBot.cs

--- Bots/DialogAndWelcomeBot.cs
+++ Bots/DialogAndWelcomeBot.cs
@@ -10,15 +10,46 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Azure;
+using System;
+using System.Linq;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
     public class DialogAndWelcomeBot<T> : DialogBot<T>
         where T : Dialog
     {
+        private BotState _conversationState;
+        private BotState _userState;
+
+        // Create local Memory Storage.
+        // private static readonly MemoryStorage _myStorage = new MemoryStorage();
+        private static readonly CosmosDbPartitionedStorage _myStorage = new CosmosDbPartitionedStorage(new CosmosDbPartitionedStorageOptions
+        {
+            CosmosDbEndpoint = "https://electioncosmos.documents.azure.com:443/",
+            AuthKey = "0zqSGejOcM7dqU603OFedsrfXf3HG6DBrwO0YZm85h2IlZrdyDY7la7tgfX0axd9ccNN4myrphorQMlxOuuBSw==",
+            DatabaseId = "BotStoage",
+            ContainerId = "Container1",
+        });
+
+        // Create cancellation token (used by Async Write operation).
+        public CancellationToken cancellationToken { get; private set; }
+
+        // Class for storing a log of utterances (text of messages) as a list.
+        public class UtteranceLog
+        {
+            // A list of things that users have said to the bot
+            public List<string> UtteranceList { get; } = new List<string>();
+
+            // The number of conversational turns that have occurred        
+            public int TurnNumber { get; set; } = 0;
+        }
+
         public DialogAndWelcomeBot(ConversationState conversationState, UserState userState, T dialog, ILogger<DialogBot<T>> logger)
             : base(conversationState, userState, dialog, logger)
         {
+            _conversationState = conversationState;
+            _userState = userState;
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
@@ -36,6 +67,77 @@ namespace Microsoft.BotBuilderSamples.Bots
                 }
             }
         }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // preserve user input.
+            var utterance = turnContext.Activity.Text;
+            // make empty local logitems list.
+            UtteranceLog logItems = null;
+
+            // see if there are previous messages saved in storage.
+            try
+            {
+                string[] utteranceList = { "UtteranceLog" };
+                logItems = _myStorage.ReadAsync<UtteranceLog>(utteranceList).Result?.FirstOrDefault().Value;
+            }
+            catch
+            {
+                // Inform the user an error occured.
+                await turnContext.SendActivityAsync("Sorry, something went wrong reading your stored messages!");
+            }
+
+            // If no stored messages were found, create and store a new entry.
+            if (logItems is null)
+            {
+                // add the current utterance to a new object.
+                logItems = new UtteranceLog();
+                logItems.UtteranceList.Add(utterance);
+                // set initial turn counter to 1.
+                logItems.TurnNumber++;
+
+                // Create Dictionary object to hold received user messages.
+                var changes = new Dictionary<string, object>();
+                {
+                    changes.Add("UtteranceLog", logItems);
+                }
+                try
+                {
+                    // Save the user message to your Storage.
+                    await _myStorage.WriteAsync(changes, cancellationToken);
+                }
+                catch
+                {
+                    // Inform the user an error occured.
+                    await turnContext.SendActivityAsync("Sorry, something went wrong storing your message!");
+                }
+            }
+            // Else, our Storage already contained saved user messages, add new one to the list.
+            else
+            {
+                // add new message to list of messages to display.
+                logItems.UtteranceList.Add(utterance);
+                // increment turn counter.
+                logItems.TurnNumber++;
+
+                // Create Dictionary object to hold new list of messages.
+                var changes = new Dictionary<string, object>();
+                {
+                    changes.Add("UtteranceLog", logItems);
+                };
+
+                try
+                {
+                    // Save new list to your Storage.
+                    await _myStorage.WriteAsync(changes, cancellationToken);
+                }
+                catch
+                {
+                    // Inform the user an error occured.
+                    await turnContext.SendActivityAsync("Sorry, something went wrong storing your message!");
+                }
+            }
+   }
 
         // Load attachment from embedded resource.
         private Attachment CreateAdaptiveCardAttachment()
