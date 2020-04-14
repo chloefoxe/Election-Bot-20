60ec915cf833053ef909a2ed0872a9852799f842
foxe22 <chloe.foxe99@gmail.com>
2020-04-14 10:38:14 +0100

    Chnaged containers

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace Microsoft.BotBuilderSamples
                 CosmosDbEndpoint = "https://electioncosmos.documents.azure.com:443/",
                 AuthKey = "0zqSGejOcM7dqU603OFedsrfXf3HG6DBrwO0YZm85h2IlZrdyDY7la7tgfX0axd9ccNN4myrphorQMlxOuuBSw==",
                 DatabaseId = "BotStoage",
-                ContainerId = "Group 1"
+                ContainerId = "Container1"
             };
             var storage = new CosmosDbPartitionedStorage(cosmosDbStorageOptions);
 
